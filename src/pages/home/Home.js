a9d4424bdb5d1c3f2afcb1af9692696d7f99b17e
SAHIL141299 <158707990+SAHIL141299@users.noreply.github.com>
2024-05-01 19:12:55 +0530

    imagecss

---
+++ src/pages/home/Home.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "../home/home.css";
+
+export default function () {
+  return (
+    <div>
+      <div className="container">
+        <div className="containerbox">box1</div>
+        <div className="container2">
+          <div className="containerbox2">box2</div>
+          <div className="container3">
+            <div className="containerbox3">box3</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
